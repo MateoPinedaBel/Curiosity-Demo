f36f0b4f7c1711e5379c1003172925649853fa35
MateoPinedaBel <MateoPinedaBel@Outlook.com>
2025-05-27 10:18:57 -0500

    refactor se cambia el main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        String nombre = "Mateo Andrés Pineda Beltrán";
+        int edad = 19;
     }
 }
